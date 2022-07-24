d874cd258ab382498a68049dd998c0d5a8527fa9
Sidra Waseem <sidraw24@gmail.com>
2022-07-24 13:19:50 +0500

    update about.php

--- about.php
+++ about.php
@@ -24,9 +24,9 @@ if(isset($_SESSION['user_id'])){
    
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
+   
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="./css/style.css">
 
 </head>
 <body>
