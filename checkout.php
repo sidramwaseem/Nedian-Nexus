24bb40791c3ca03adf6130378a9e292384c1cbd7
Sidra Waseem <sidraw24@gmail.com>
2022-07-26 12:35:39 +0500

    update checkout.php

--- checkout.php
+++ checkout.php
@@ -4,14 +4,14 @@ include 'components/connect.php';
 
 session_start();
 
-if(isset($_SESSION['user_id'])){
+if (isset($_SESSION['user_id'])) {
    $user_id = $_SESSION['user_id'];
-}else{
+} else {
    $user_id = '';
    header('location:user_login.php');
 };
 
-if(isset($_POST['order'])){
+if (isset($_POST['order'])) {
 
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
@@ -21,7 +21,7 @@ if(isset($_POST['order'])){
    $email = filter_var($email, FILTER_SANITIZE_STRING);
    $method = $_POST['method'];
    $method = filter_var($method, FILTER_SANITIZE_STRING);
-   $address = 'flat no. '. $_POST['flat'] .', '. $_POST['street'] .', '. $_POST['city'] .', '. $_POST['state'] .', '. $_POST['country'] .' - '. $_POST['pin_code'];
+   $address = 'flat no. ' . $_POST['flat'] . ', ' . $_POST['street'] . ', ' . $_POST['city'] . ', ' . $_POST['state'] . ', ' . $_POST['country'] . ' - ' . $_POST['pin_code'];
    $address = filter_var($address, FILTER_SANITIZE_STRING);
    $total_products = $_POST['total_products'];
    $total_price = $_POST['total_price'];
@@ -29,7 +29,7 @@ if(isset($_POST['order'])){
    $check_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
    $check_cart->execute([$user_id]);
 
-   if($check_cart->rowCount() > 0){
+   if ($check_cart->rowCount() > 0) {
 
       $insert_order = $conn->prepare("INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price) VALUES(?,?,?,?,?,?,?,?)");
       $insert_order->execute([$user_id, $name, $number, $email, $method, $address, $total_products, $total_price]);
@@ -38,22 +38,22 @@ if(isset($_POST['order'])){
       $delete_cart->execute([$user_id]);
 
       $message[] = 'order placed successfully!';
-   }else{
+   } else {
       $message[] = 'your cart is empty';
    }
-
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>checkout</title>
-   
+
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
@@ -61,95 +61,96 @@ if(isset($_POST['order'])){
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<?php include 'components/user_header.php'; ?>
 
-<section class="checkout-orders">
+   <?php include 'components/user_header.php'; ?>
 
-   <form action="" method="POST">
+   <section class="checkout-orders">
 
-   <h3>your orders</h3>
+      <form action="" method="POST">
 
-      <div class="display-orders">
-      <?php
-         $grand_total = 0;
-         $cart_items[] = '';
-         $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
-         $select_cart->execute([$user_id]);
-         if($select_cart->rowCount() > 0){
-            while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
-               $cart_items[] = $fetch_cart['name'].' ('.$fetch_cart['price'].' x '. $fetch_cart['quantity'].') - ';
-               $total_products = implode($cart_items);
-               $grand_total += ($fetch_cart['price'] * $fetch_cart['quantity']);
-      ?>
-         <p> <?= $fetch_cart['name']; ?> <span>(<?= '$'.$fetch_cart['price'].'/- x '. $fetch_cart['quantity']; ?>)</span> </p>
-      <?php
+         <h3>your orders</h3>
+
+         <div class="display-orders">
+            <?php
+            $grand_total = 0;
+            $cart_items[] = '';
+            $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+            $select_cart->execute([$user_id]);
+            if ($select_cart->rowCount() > 0) {
+               while ($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)) {
+                  $cart_items[] = $fetch_cart['name'] . ' (' . $fetch_cart['price'] . ' x ' . $fetch_cart['quantity'] . ') - ';
+                  $total_products = implode($cart_items);
+                  $grand_total += ($fetch_cart['price'] * $fetch_cart['quantity']);
+            ?>
+                  <p> <?= $fetch_cart['name']; ?> <span>(<?= '$' . $fetch_cart['price'] . '/- x ' . $fetch_cart['quantity']; ?>)</span> </p>
+            <?php
+               }
+            } else {
+               echo '<p class="empty">your cart is empty!</p>';
             }
-         }else{
-            echo '<p class="empty">your cart is empty!</p>';
-         }
-      ?>
-         <input type="hidden" name="total_products" value="<?= $total_products; ?>">
-         <input type="hidden" name="total_price" value="<?= $grand_total; ?>" value="">
-         <div class="grand-total">grand total : <span>$<?= $grand_total; ?>/-</span></div>
-      </div>
-
-      <h3>place your orders</h3>
-
-      <div class="flex">
-         <div class="inputBox">
-            <span>your name :</span>
-            <input type="text" name="name" placeholder="enter your name" class="box" maxlength="20" required>
+            ?>
+            <input type="hidden" name="total_products" value="<?= $total_products; ?>">
+            <input type="hidden" name="total_price" value="<?= $grand_total; ?>" value="">
+            <div class="grand-total">grand total : <span>$<?= $grand_total; ?>/-</span></div>
          </div>
-         <div class="inputBox">
-            <span>your number :</span>
-            <input type="number" name="number" placeholder="enter your number" class="box" min="0" max="9999999999" onkeypress="if(this.value.length == 10) return false;" required>
+
+         <h3>place your orders</h3>
+
+         <div class="flex">
+            <div class="inputBox">
+               <span>your name :</span>
+               <input type="text" name="name" placeholder="enter your name" class="box" maxlength="20" required>
+            </div>
+            <div class="inputBox">
+               <span>your number :</span>
+               <input type="number" name="number" placeholder="enter your number" class="box" min="0" max="9999999999" onkeypress="if(this.value.length == 10) return false;" required>
+            </div>
+            <div class="inputBox">
+               <span>your email :</span>
+               <input type="email" name="email" placeholder="enter your email" class="box" maxlength="50" required>
+            </div>
+            <div class="inputBox">
+               <span>payment method :</span>
+               <select name="method" class="box" required>
+                  <option value="cash on delivery">cash on delivery</option>
+                  <option value="credit card">credit card</option>
+                  <option value="paytm">paytm</option>
+                  <option value="paypal">paypal</option>
+               </select>
+            </div>
+            <div class="inputBox">
+               <span>address line 01 :</span>
+               <input type="text" name="flat" placeholder="e.g. flat number" class="box" maxlength="50" required>
+            </div>
+            <div class="inputBox">
+               <span>address line 02 :</span>
+               <input type="text" name="street" placeholder="e.g. street name" class="box" maxlength="50" required>
+            </div>
+            <div class="inputBox">
+               <span>city :</span>
+               <input type="text" name="city" placeholder="e.g. mumbai" class="box" maxlength="50" required>
+            </div>
+            <div class="inputBox">
+               <span>state :</span>
+               <input type="text" name="state" placeholder="e.g. maharashtra" class="box" maxlength="50" required>
+            </div>
+            <div class="inputBox">
+               <span>country :</span>
+               <input type="text" name="country" placeholder="e.g. India" class="box" maxlength="50" required>
+            </div>
+            <div class="inputBox">
+               <span>pin code :</span>
+               <input type="number" min="0" name="pin_code" placeholder="e.g. 123456" min="0" max="999999" onkeypress="if(this.value.length == 6) return false;" class="box" required>
+            </div>
          </div>
-         <div class="inputBox">
-            <span>your email :</span>
-            <input type="email" name="email" placeholder="enter your email" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>payment method :</span>
-            <select name="method" class="box" required>
-               <option value="cash on delivery">cash on delivery</option>
-               <option value="credit card">credit card</option>
-               <option value="paytm">paytm</option>
-               <option value="paypal">paypal</option>
-            </select>
-         </div>
-         <div class="inputBox">
-            <span>address line 01 :</span>
-            <input type="text" name="flat" placeholder="e.g. flat number" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>address line 02 :</span>
-            <input type="text" name="street" placeholder="e.g. street name" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>city :</span>
-            <input type="text" name="city" placeholder="e.g. mumbai" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>state :</span>
-            <input type="text" name="state" placeholder="e.g. maharashtra" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>country :</span>
-            <input type="text" name="country" placeholder="e.g. India" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>pin code :</span>
-            <input type="number" min="0" name="pin_code" placeholder="e.g. 123456" min="0" max="999999" onkeypress="if(this.value.length == 6) return false;" class="box" required>
-         </div>
-      </div>
 
-      <input type="submit" name="order" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>" value="place order">
+         <input type="submit" name="order" class="btn <?= ($grand_total > 1) ? '' : 'disabled'; ?>" value="place order">
 
-   </form>
+      </form>
 
-</section>
+   </section>
 
 
 
@@ -163,9 +164,10 @@ if(isset($_POST['order'])){
 
 
 
-<?php include 'components/footer.php'; ?>
+   <?php include 'components/footer.php'; ?>
 
-<script src="js/script.js"></script>
+   <script src="js/script.js"></script>
 
 </body>
+
 </html>
