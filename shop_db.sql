d0572284275ee235b1a752fe2b5c919777d64055
Sidra Waseem <sidraw24@gmail.com>
2022-07-26 12:38:25 +0500

    format shop_db.sql

--- shop_db.sql
+++ shop_db.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 30, 2022 at 12:51 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `shop_db`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `admins`
---
 
 CREATE TABLE `admins` (
   `id` int(100) NOT NULL,
