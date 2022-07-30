d44234052516cda2d1080aeb404a3c5a07ab75ac
Sidra Waseem <sidraw24@gmail.com>
2022-07-30 16:35:09 +0500

    update home page

--- home.php
+++ home.php
@@ -29,7 +29,7 @@ include 'wishlist_cart.php';
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="./style.css">
 
 </head>
 
