6d73441b2eb2ba19bda19f72e90e119c4e60d30e
Sidra Waseem <sidraw24@gmail.com>
2022-08-02 10:13:46 +0500

    update uswer_register.php

--- user_register.php
+++ user_register.php
@@ -65,12 +65,12 @@ if(isset($_POST['submit'])){
    <form action="" method="post">
       <h3>register now</h3>
       <input type="text" name="name" required placeholder="enter your username" maxlength="20"  class="box">
-      <input type="email" name="email" required placeholder="enter your email:@cloud.neduet.edu.pk" pattern=".+@cloud.neduet.edu.pk" maxlength="500"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="email" name="email" required placeholder="email@cloud.neduet.edu.pk" pattern=".+@cloud.neduet.edu.pk" maxlength="500"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
       <input type="password" name="pass" required placeholder="enter your password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
       <input type="password" name="cpass" required placeholder="confirm your password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
       <input type="submit" value="register now" class="btn" name="submit">
-      <p>already have an account?</p>
-      <a href="user_login.php" class="option-btn">login now</a>
+      <p>Already have an account?</p>
+      <a href="user_login.php" class="option-btn">Login now</a>
    </form>
 
 </section>
